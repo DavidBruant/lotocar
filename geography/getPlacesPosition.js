14e72a87e88a6f9345b9c668002be7f8a7de9ea4
David Bruant <bruant.d@gmail.com>
2019-10-24 12:06:27 +0200

    Retourne le résultat :-)

--- geography/getPlacesPosition.js
+++ geography/getPlacesPosition.js
@@ -6,7 +6,7 @@ export default function(places){
 
     // places can be a large set, which in turn may lead to a large URL
     // splitting the set so we don't run into "too large URL" HTTP errors
-    Promise.all(chunk([...places], 100).map(somePlaces => {
+    return Promise.all(chunk([...places], 100).map(somePlaces => {
         return json(`/positions?${(somePlaces.map(p => `p=${encodeURIComponent(p)}`).join('&'))}`)
     }))
     .then(chunksResults => {
