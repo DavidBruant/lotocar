fbaeaf16c7ce58c31e2e6b8a00f24b411358ad3a
Mael <mael.thomas@data.gouv.fr>
2019-11-04 15:17:58 +0100

    Affichage des trajets uniquement après une recherche

--- direct/components/DriverList.js
+++ direct/components/DriverList.js
@@ -83,7 +83,7 @@ const StandardContact = ({}) => {
 
 export default function DriversList({
 	tripProposalsByTrip,
-	tripRequest,
+	validTripRequest,
 	tripDetailsByTrip,
 	displayedDriverTrips,
 	onTripClick
@@ -104,6 +104,18 @@ export default function DriversList({
 		return detour1 - detour2
 	})
 
+	if (!orderedTrips.length) return null
+	if (!validTripRequest)
+		return html`
+			<div style=${{ textAlign: 'center', marginTop: '2rem' }}>
+				<p style=${{ marginBottom: '0rem' }}>
+					${orderedTrips.length} trajets disponibles sur Lotocar
+				</p>
+				<a href="http://bit.ly/inscription-conducteur"
+					>J'ai une voiture et je veux aider</a
+				>
+			</div>
+		`
 	return html`
 		<h2 key="h2">Conducteur.rice.s</h2>
 		<ul key="ul" className="drivers-list">
